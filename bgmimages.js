574a06290a3a8258080b0ad9669a30efa44dfa4a
ChaimWieder <113867156+ChaimWieder@users.noreply.github.com>
2025-12-18 19:29:36 +0000

    Revert "Update bgmimages.js"
    This reverts commit d9e1e56c251b7b6586604290ea6c0f52891cc214.

--- bgmimages.js
+++ bgmimages.js
@@ -2,13 +2,12 @@ function FindProxyForURL(url, host) {
 
     // Block Google Maps images (Places photos)
     if (
-        dnsDomainIs(host, "streetviewpixels-pa.googleapis.com") //||
-        // dnsDomainIs(host, "lh3.googleusercontent.com") ||
-        // dnsDomainIs(host, "lh4.googleusercontent.com") ||
-        // dnsDomainIs(host, "lh5.googleusercontent.com") ||
-        // dnsDomainIs(host, "lh6.googleusercontent.com") ||
-        // dnsDomainIs(host, "places.googleapis.com") ||
-        // shExpMatch(url, "*maps.googleapis.com/maps/api/place*")
+        dnsDomainIs(host, "lh3.googleusercontent.com") ||
+        dnsDomainIs(host, "lh4.googleusercontent.com") ||
+        dnsDomainIs(host, "lh5.googleusercontent.com") ||
+        dnsDomainIs(host, "lh6.googleusercontent.com") ||
+        dnsDomainIs(host, "places.googleapis.com") ||
+        shExpMatch(url, "*maps.googleapis.com/maps/api/place*")
     ) {
         return "PROXY 0.0.0.0:0"; 
     }
